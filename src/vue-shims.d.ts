4c52c5ff172ec340e4e78b24cf820d977a16cff0
Nikolai <nikolai.laevskii@yandex.ru>
2018-10-31 02:03:36 +0300

    типы модулей вынесены в отдельный файл

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -2,13 +2,3 @@ declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
-
-declare module '*.svg' {
-  const url: string;
-  export default url;
-}
-
-declare module '*.jpg' {
-  const url: string;
-  export default url;
-}
